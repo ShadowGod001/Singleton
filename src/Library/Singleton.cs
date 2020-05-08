b5bdbf7e441bc4fc1abe54e783d0b9e79f8b0b31
ShadowGod001 <55005167+ShadowGod001@users.noreply.github.com>
2020-05-08 19:13:35 -0300

    Ejercicio

---
+++ src/Library/Singleton.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Library
+{
+    public class Singleton<T> where T : new()
+    {
+        private Singleton ()
+        {
+            
+        }
+
+        private static T instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new T();
+                }
+
+                return instance;
+            }
+        }
+    }
+
+}
